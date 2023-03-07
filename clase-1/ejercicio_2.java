fe9c526601a6794b92300d8eb210b606fa5cc92b
Kadyr Valdes <valdeskadyr@gmail.com>
2023-03-07 14:44:32 -0300

    commit inicial

---
+++ clase-1/ejercicio_2.java
@@ -0,0 +1,22 @@
+public class ejercicio_2 {
+    public static void main(String[] args) {
+        income(56, 10, 1, false);
+    }
+
+
+    public static void income(int income, int cantidadVehiculos, int cantidadInmuebles, boolean otrosActivos) {
+
+        // if income is more iqual than 489083 or more than 3 vehicles or more than 2 properties or other assets then declare as a high income
+        if (income >= 489083 || cantidadVehiculos > 2 || cantidadInmuebles > 2 || otrosActivos == true) {
+            System.out.println("El ingreso es alto");
+        }
+        else {
+            System.out.println("El ingreso es bajo");
+        }
+    }
+
+
+
+
+
+}
