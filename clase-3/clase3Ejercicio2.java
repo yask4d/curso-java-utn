0da2e90085a3ada297417b7ee620abfdf3a53f72
Kadyr Valdes <valdeskadyr@gmail.com>
2023-04-02 13:46:26 -0300

    fix: dejar espacios iguales

--- clase-3/clase3Ejercicio2.java
+++ clase-3/clase3Ejercicio2.java
@@ -2,8 +2,8 @@ public class clase3Ejercicio2 {
     // main
     public static void main(String[] args) {
         // codificar una cadena con un desplazamiento de 1 caracter hacia la derecha, y teniendo en cuenta que la z se convierte en a
-        System.out.println(codificar1("holax yzz")); 
-        System.out.println(codificar2("hola xyz"));
+        System.out.println(codificar1("holax yzz que tal")); 
+        System.out.println(codificar2("hola xyz que tal"));
     } 
     
      public static String codificar1(String texto) {
@@ -14,9 +14,13 @@ public class clase3Ejercicio2 {
             if (caracter == 'z') {
                 caracter = 'a';
             }
-            else {
-                caracter = (char) (caracter + desplazamiento);
+            // dejar igual el caracter si es un espacio
+            else if (caracter == ' ') {
+                caracter = ' ';
             }
+            else {                
+                caracter = (char) (caracter + desplazamiento);
+            }            
             textoCodificado = textoCodificado + caracter;
         }
         return textoCodificado;    
@@ -32,6 +36,9 @@ public class clase3Ejercicio2 {
             }
             else if (caracter == 'z') {
                 caracter = 'b';
+            }
+            else if (caracter == ' ') {
+                caracter = ' ';
             }
             else {                
                 caracter = (char) (caracter + desplazamiento);
