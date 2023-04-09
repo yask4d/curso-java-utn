e256f2f85e74350e32fe111b81b05c8fb786446e
Kadyr Valdes <valdeskadyr@gmail.com>
2023-04-09 16:03:18 -0300

    debug

--- tp/src/main/java/utn/tp/mainTP.java
+++ tp/src/main/java/utn/tp/mainTP.java
@@ -1,9 +1,27 @@
 package utn.tp;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class mainTP {
     // metodo main
     public static void main(String[] args) {
-        // TODO Auto-generated method stub       
+        // leer archivo de resultados.csv usando paths.get guardarlo en una lista
+        
+        Path pathResultados = Paths.get(null, "./src/test/resources/resultados.csv");
+        // iterar linea a linea pathResultados
+        for (Path path : pathResultados) {
+            // imprimir path
+            System.out.println(path);
+            
+        }
+
+        
+
+
+
+        
+
     
     }
 }
